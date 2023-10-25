da3f9a6078c24d21d230d197549cb83f55474b83
Karely Valdés <karely_valdes07@live.com>
2023-10-24 22:46:50 -0600

    cambios perfil

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -5,11 +5,73 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          "Profile Screen"
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 10, left: 10),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Perfil",
+                  style: TextStyle(
+                    fontSize: 27,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 40,),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage("assets/nike1.jpg"),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blue,
+                    ),
+                    child: IconButton(
+                      icon: const Icon(Icons.camera_alt, color: Colors.white),
+                      onPressed: () {
+                        // Lógica para cambiar la foto de perfil
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            _buildInfoBox("Nombre de Usuario", Icons.person),
+            _buildInfoBox("Correo electrónico: usuario@example.com", Icons.email),
+            _buildInfoBox("Número de teléfono: +123 456 7890", Icons.phone),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildInfoBox(String text, IconData icon) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blue),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.blue),
+          const SizedBox(width: 10),
+          Text(text, style: const TextStyle(fontSize: 16)),
+        ],
       ),
     );
   }
